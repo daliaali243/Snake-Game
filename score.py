da180ca7bfdc0f273d01d4baf2bb43c9651a2699
daliaali243 <daliaali2214@gmail.com>
2025-10-17 15:11:09 +0300

    Implement high score tracking in Score class
    Added functionality to save and display high score.

--- score.py
+++ score.py
@@ -5,6 +5,7 @@ class Score(Turtle):
         self.penup()
         self.goto(0, 350)
         self.score = 0
+        self.high_score=self.saved_high_score()
         self.color('black')
         self.update_board()
         self.hideturtle()
@@ -15,8 +16,18 @@ class Score(Turtle):
         self.clear()
         self.score += 1
         self.update_board()
+    def saved_high_score(self):
+        with open('high_score.txt','r') as file:
+            return int(file.read())
+    def increase_high_score(self):
+        with open('high_score.txt','w') as file:
+            file.write(str(self.high_score))
     def game_over(self):
         self.screen.bgcolor('red')
         self.goto(0, 0)
         self.hideturtle()
-        self.write(f'     game over\nyour final score is {self.score}', align='center', font=('Arial', 20, 'bold'))
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.increase_high_score()
+
+        self.write(f'     game over\nyour final score is {self.score}\nhigh score:{self.high_score}', align='center', font=('Arial', 20, 'bold'))
